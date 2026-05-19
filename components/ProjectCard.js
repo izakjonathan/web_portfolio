453a4792cf0f78836498c95d2c1be342d8d659e1
Izak Hyllested <55657459+izakjonathan@users.noreply.github.com>
2026-05-19 23:37:33 +0200

    h

--- components/ProjectCard.js
+++
@@ -1,9 +0,0 @@
-export default function ProjectCard({ project }) {
-  return (
-    <article className="card">
-      <div className="kicker">{project.category}</div>
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
-    </article>
-  );
-}
