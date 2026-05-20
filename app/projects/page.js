de15dbac00335894d90ee34ae83b9fb4c7c81a27
Izak Hyllested <55657459+izakjonathan@users.noreply.github.com>
2026-05-20 13:45:07 +0200

    g

--- app/projects/page.js
+++
@@ -1,7 +0,0 @@
-export default function ProjectsPage() {
-  return (
-    <main style={{ padding: '120px 24px', color: 'white' }}>
-      <h1>Projects</h1>
-    </main>
-  );
-}
--- components/ProjectRow.js
+++
@@ -1,8 +0,0 @@
-export default function ProjectRow({ project }) {
-  return (
-    <div className="project-row">
-      <h2>{project.title}</h2>
-      <p>{project.description}</p>
-    </div>
-  );
-}
