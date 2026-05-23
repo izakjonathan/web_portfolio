48e7c6df677a9df155580f0bdb5403a7bb18c4b9
Izak Hyllested <55657459+izakjonathan@users.noreply.github.com>
2026-05-23 10:56:20 +0200

    w

---
+++ components/Nav.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <>
+      <header className="nav blend-layer">
+        <div className="nav-inner">
+          <Link href="/" className="nav-logo">IZAK HYLLESTED</Link>
+          <a href="#about">ABOUT</a>
+        </div>
+      </header>
+
+      <footer className="footer-nav blend-layer">
+        <div className="footer-inner">
+          <a href="mailto:izakhyllested@icloud.com">CONTACT</a>
+          <Link href="/projects">PROJECTS</Link>
+        </div>
+      </footer>
+    </>
+  );
+}
